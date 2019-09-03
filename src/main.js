192d50083df07c60525c58f961940ae3adefa2d1
Nicole-Lastra <nico.lastrasanchez@gmail.com>
2019-09-03 09:19:45 -0300

    HU Ordenar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-/* Manejo del DOM */
+const listpokemon= POKEMON.pokemon;
+
+for(let i = 0; i < listpokemon.length; i++){
+
+  const cont = document.getElementById("root");// crear un contenedor
+
+  const imagenpokemon=document.createElement("img");
+  imagenpokemon.src= listpokemon[i].img;
+
+  let numpokemon= document.createElement("p");
+  numpokemon.textContent= listpokemon[i].num;
+
+  let namepokemon= document.createElement("h2");
+  namepokemon.textContent= listpokemon[i].name;
+
+ cont.appendChild(imagenpokemon);
+ cont.appendChild(numpokemon);
+ cont.appendChild(namepokemon);
+
+ cont.innerHTML;
+
+
+
+}
