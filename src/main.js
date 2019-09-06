c925354d345a9b1e27f6bc1fb705b35e1fbf6efb
Nicole-Lastra <nico.lastrasanchez@gmail.com>
2019-09-06 12:32:51 -0300

    HTML y Javascript corregidos

--- src/main.js
+++ src/main.js
@@ -23,7 +23,6 @@ for(let i = 0; i < listpokemon.length; i++){
 // se crea el elemento nombre
   let namepokemon= document.createElement("h1");
   namepokemon.textContent= listpokemon[i].name;
-  namepokemon.className = "title";
 
 //se crean los elementos hijos en el contenedor padre
  contdata.appendChild(imagenpokemon);
@@ -34,33 +33,40 @@ for(let i = 0; i < listpokemon.length; i++){
 }
 
 // filtrado por tipo
+
 const buttonfilter= document.getElementById("btnType");
 buttonfilter.addEventListener('click',()=>{
-
+//Toma el valor del select
+    let typetext=document.getElementById("typepokemon").value;
+       
+//se realiza el filtrado y muestra todo el objeto
     let pokemonfilter =listpokemon.filter(element=>{
-        return element.type=='Poison';
+        
+        return element.type == typetext;
     })
+
+    //se guarda la constante para imprimir el el contenedor root
     
     const showfilterpokemon= document.getElementById("root");
 
+    // se recorre cada elemento del objeto y se imprimen en el contenedor de los personajes
+
      showfilterpokemon.innerHTML= pokemonfilter.map (element => {
-     // return element.spawn_chance;
-      return  ` <div class="flex-container">
+    
+      return  `<div class="flex-container">
                  <img src="${element.img}">
-                 <h3> # ${element.num}</h3>
+                 <a href=""> # ${element.num}</a>
                  <h1> ${element.name}</h1>
                  </div>`
              
     }).join("");
 
-    //console.log(showfilterpokemon1);
-    //showfilterpokemon1.innerHTML = showfilterpokemon;
+    
 });
 
+const returnhome= document.getElementById("return");
+returnhome.addEventListener('click',()=>{
 
+    location.reload();
 
-
-
-=======
-const pokemonfilter= listpokemon.filter(x => x.type=='Poison' );
-console.log(pokemonfilter);
+});
